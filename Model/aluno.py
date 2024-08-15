9654baf2ad25fd6f1ffc199a90ef030ef2d793ed
Rafael <rafael.florentino.barbosa@gmail.com>
2024-08-15 17:45:49 -0300

    connecting to the database

---
+++ Model/aluno.py
@@ -0,0 +1,6 @@
+class Aluno:
+    def __init__(self, ra, nome, tempoestudo, rendafamiliar):
+        self.ra = ra
+        self.nome = nome
+        self.tempoestudo = tempoestudo
+        self.rendafamiliar = rendafamiliar
--- main.py
+++ main.py
@@ -1,7 +1,43 @@
-from flask import Flask, render_template, request
-from markupsafe import escape
+from flask import Flask,render_template,request
+
+from flask_sqlalchemy import SQLAlchemy
+
+# Importa a função `sessionmaker`, que é usada para criar uma nova sessão para interagir com o banco de dados
+from sqlalchemy.orm import sessionmaker
+
+# Importa as funções `create_engine` para estabelecer uma conexão com o banco de dados e `MetaData` para trabalhar com metadados do banco de dados
+from sqlalchemy import create_engine, MetaData
+
+# Importa a função `automap_base`, que é usada para refletir um banco de dados existente em classes ORM automaticamente
+from sqlalchemy.ext.automap import automap_base
+from Model.aluno import Aluno
+
+import urllib.parse
 
 app = Flask(__name__)
+
+user = 'root'
+password = urllib.parse.quote_plus('senai@123')
+
+host = 'localhost'
+database = 'schooltracker'
+connection_string = f'mysql+pymysql://{user}:{password}@{host}/{database}'
+
+# Criar a engine e refletir o banco de dados existente
+engine = create_engine(connection_string)
+metadata = MetaData()
+metadata.reflect(engine)
+
+# Mapeamento automático das tabelas para classes Python
+Base = automap_base(metadata=metadata)
+Base.prepare()
+
+# Acessando a tabela 'aluno' mapeada
+Aluno = Base.classes.aluno
+
+# Criar a sessão do SQLAlchemy
+Session = sessionmaker(bind=engine)
+session = Session()
 
 @app.route("/")
 def index():
@@ -15,11 +51,38 @@ def cadastro():
 def inserir_aluno():
     ra = request.form['ra']
     nome = request.form['nome']
-    tempoestudo = request.form['tempoestudo']
-    rendafamiliar = request.form['rendafamiliar']
+    tempoestudo = int(request.form['tempoestudo'])
+    rendafamiliar = float(request.form['rendafamiliar'])
+
+    # Verifica se o RA já existe no banco de dados
+    aluno_existente = session.query(Aluno).filter_by(ra=ra).first()
+
+    if aluno_existente:
+        mensagem = "RA já cadastrado no sistema."
+        return render_template('index.html', msgbanco=mensagem)
+
+    aluno = Aluno(ra=ra,nome=nome,tempoestudo=tempoestudo,rendafamiliar=rendafamiliar)
     
-    return render_template("index.html", ra=ra)
+    try:
+        session.add(aluno) 
+        session.commit() 
+    except:
+        session.rollback()
+        raise 
+    finally:
+        session.close()
+    mensagem = "cadastro efetuado com sucesso"
+    return render_template('index.html',msgbanco=mensagem)
+
+@app.route('/logar',methods=['POST'])
+def logar_ra():
+    ra = request.form['ra']
+    aluno_existente = session.query(Aluno).filter_by(ra=ra).first()
+    if ra == aluno_existente:
+        return render_template('diariobordo.html',ra=ra)
+    else:
+        mensagem = "ra inválido"
+        return render_template('index.html',mensagem=mensagem)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
